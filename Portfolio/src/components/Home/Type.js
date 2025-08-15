8d6c9e2b4a0e9d52c35b495b453b34a9156b1a15
roshanimaurya.in@gmail.com <avasheshmaurya419@gmail.com>
2025-08-15 10:09:48 +0530

    changes

--- Portfolio/src/components/Home/Type.js
+++ Portfolio/src/components/Home/Type.js
@@ -8,7 +8,8 @@ function Type() {
         strings: [
           "Software Developer",
           "frontend Developer",
-          "backend Developer"
+          "backend Developer",
+          "MREN Stack Developer",
         ],
         autoStart: true,
         loop: true,
--- Portfolio/src/components/Resume/ResumeNew.js
+++ Portfolio/src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/roshani_resume.pdf";
+import pdf from "../../Assets/../Assets/Resume.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
@@ -27,27 +27,17 @@ function ResumeNew() {
             style={{ maxWidth: "250px" }}
           >
             <AiOutlineDownload />
-            &nbsp;Download CV
+            &nbsp;Download Resume
           </Button>
         </Row>
 
         <Row className="resume">
-          <Document file={pdf} className="d-flex justify-content-center">
+          <Document file={pdf}  className="d-flex justify-content-center">
             <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
           </Document>
         </Row>
 
-        <Row style={{ justifyContent: "center", position: "relative" }}>
-          <Button
-            variant="primary"
-            href={pdf}
-            target="_blank"
-            style={{ maxWidth: "250px" }}
-          >
-            <AiOutlineDownload />
-            &nbsp;Download CV
-          </Button>
-        </Row>
+   
       </Container>
     </div>
   );
